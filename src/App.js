58eda34985f3c41e469b25e72340f8a2d0abd79e
ryamay <yamamoto.ryuji.dev@gmail.com>
2024-08-11 00:26:06 +0900

    fix(app): 履歴ボタンのli要素にkeyを追加

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ export default function Game() {
       description = 'Go to game start';
     }
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
