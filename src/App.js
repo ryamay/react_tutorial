9f71e0aa5672c92e9123090ef6d75f29c32ef623
ryamay <yamamoto.ryuji.dev@gmail.com>
2024-08-10 23:24:34 +0900

    feat(app): 記入済みのSquareは変更しないよう修正

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i] != null) {
+      return;
+    }
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = 'X';
