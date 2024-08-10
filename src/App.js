8a82121fbf8d24b07d61c1fbdd4ba6d217b6e455
ryamay <yamamoto.ryuji.dev@gmail.com>
2024-08-11 00:36:20 +0900

    fix(app): 履歴ボタンクリック時、ボードを更新するよう修正

--- src/App.js
+++ src/App.js
@@ -75,17 +75,21 @@ function judgeWinner(squares) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
-    // TODO
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
 
+  const currentSquares = history[currentMove];
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
@@ -99,7 +103,6 @@ export default function Game() {
       </li>
     );
   });
-
   return (
     <div className='game'>
       <div className='game-board'>
